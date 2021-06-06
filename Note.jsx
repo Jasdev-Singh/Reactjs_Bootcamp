47c7cfa20f6b80755d98224f4ad07bc3835f3c8b
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2021-06-06 13:54:29 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="contentpart">
+      <h2 className="cnthead">Javascript and react.js bootcamp</h2>
+      <p className="cntpar">
+        This was an amazing bootcamp. I learned so many new thing using the
+        concept of javascript,react.js,html and jsx.
+      </p>
+    </div>
+  );
+}
+export default Note;
